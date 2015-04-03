1d779cf9e5d81717749fdc2050fadb23420870a7
ktantry <kushaltantry@gmail.com>
2015-04-03 10:39:17 -0700

    Initial Commit

---
+++ js/quizApp.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+	$('body').css('background-color', 'green');
+
+	var clickCount = 0;
+
+		$('.button').mousedown(function() {
+			clickCount++;
+			if(clickCount == 1) {
+				$('button').text('submit');
+				$('h2').text('Which male tennis player has won the most grand slams?');
+				$('h2').css('margin-top', '300px');
+				$('h1').hide();
+				$('.instructions').text('Submit your answer below');
+
+			$(".instructions").after("<div class = 'question-list-1'><ul>"+
+				"<li class='question-a'><input type='checkbox'>A. Rafael Nadal</li>"+
+				"<li class='question-b'><input type='checkbox'>B. John McEnroe</li>"+
+				"<li class='question-c'><input type='checkbox'>C. Roger Federer</li>"+
+				"<li class='question-d'><input type='checkbox'>D. Brian Scalabrine</li>"+
+				"</ul></div>");
+		}
+			else if(clickCount == 2) {
+				$('.question-list-1').hide();
+				$('button').text('submit');
+				$('h2').text('Which female tennis player has won the most grand slams?');
+				$('h2').css('margin-top', '300px');
+				$('h1').hide();
+				$('.instructions').text('Submit your answer below');
+
+			$(".instructions").after("<div class = 'question-list-2'><ul>"+
+				"<li class='question-a'><input type='checkbox'>A. Margart Court</li>"+
+				"<li class='question-b'><input type='checkbox'>B. Serena Williams</li>"+
+				"<li class='question-c'><input type='checkbox'>C. Martina Navratilova</li>"+
+				"<li class='question-d'><input type='checkbox'>D. Brian Scalabrine</li>"+
+				"</ul></div>");
+		}
+			else if(clickCount == 3) {
+				$('.question-list-1').hide();
+				$('.question-list-2').hide();
+				$('button').text('submit');
+				$('h2').text('Which male tennis player has won the most French Opens?');
+				$('h2').css('margin-top', '300px');
+				$('h1').hide();
+				$('.instructions').text('Submit your answer below');
+
+			$(".instructions").after("<div class = 'question-list-3'><ul>"+
+				"<li class='question-a'><input type='checkbox'>A. Rafael Nadal</li>"+
+				"<li class='question-b'><input type='checkbox'>B. Roger Federer</li>"+
+				"<li class='question-c'><input type='checkbox'>C. Guillermo Coria</li>"+
+				"<li class='question-d'><input type='checkbox'>D. Brian Scalabrine</li>"+
+				"</ul></div>");
+		}
+
+			else if(clickCount == 4) {
+				$('.question-list-1').hide();
+				$('.question-list-2').hide();
+				$('.question-list-3').hide();
+				$('button').text('submit');
+				$('h2').text('Who won the Australian Open in 2015?');
+				$('h2').css('margin-top', '300px');
+				$('h1').hide();
+				$('.instructions').text('Submit your answer below');
+
+			$(".instructions").after("<div class = 'question-list-4'><ul>"+
+				"<li class='question-a'><input type='checkbox'>A. Rafael Nadal</li>"+
+				"<li class='question-b'><input type='checkbox'>B. Roger Federer</li>"+
+				"<li class='question-c'><input type='checkbox'>C. Novak Djokovic</li>"+
+				"<li class='question-d'><input type='checkbox'>D. Brian Scalabrine</li>"+
+				"</ul></div>");
+		}
+
+			else if(clickCount == 5) {
+				$('.question-list-1').hide();
+				$('.question-list-2').hide();
+				$('.question-list-3').hide();
+				$('.question-list-4').hide();
+				$('button').text('submit');
+				$('h2').text("What is Nadal's head to head record vs. Federer");
+				$('h2').css('margin-top', '300px');
+				$('h1').hide();
+				$('.instructions').text('Submit your answer below');
+
+			$(".instructions").after("<div class = 'question-list-4'><ul>"+
+				"<li class='question-a'><input type='checkbox'>A. 1-23</li>"+
+				"<li class='question-b'><input type='checkbox'>B. 10-11</li>"+
+				"<li class='question-c'><input type='checkbox'>C. 23-10</li>"+
+				"<li class='question-d'><input type='checkbox'>D. Brian Scalabrine</li>"+
+				"</ul></div>");
+		}
+
+			else if(clickCount == 6) {
+				$('.question-list-1').hide();
+				$('.question-list-2').hide();
+				$('.question-list-3').hide();
+				$('.question-list-4').hide();
+				$('.question-list-5').hide();
+				$('button').hide();
+				$('h2').hide();
+				$('.instructions').hide();
+				$('h1').show().animate();
+				$('h1').text('Congratulations! You answered X out of 5 questions correctly.')
+				
+		}
+
+
+		});
+
+
+});
